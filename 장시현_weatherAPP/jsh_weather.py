61f9f01a27624c7a3ce8ac8c66f43a4da8c5fb04
jangsihyeon <42333610jsh@gmail.com>
2023-03-20 16:59:55 +0900

    .
    value로 분류

--- 장시현_weatherAPP/jsh_weather.py
+++ 장시현_weatherAPP/jsh_weather.py
@@ -9,10 +9,10 @@ api_url = 'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtN
 querySTR= '?'+urlencode(
 {
     'serviceKey' : unquote('4n4Miwzm5p37SLb9Jk9bJa%2FMhFYSTl8mkQIensYxsOuwWyjpePzkk6oyRp3pOsd8GVnzwwQelKHMwSc0bPVfSA%3D%3D'), 
-    'pageNo' : '1', 
-    'numOfRows' : '700', 
+    'pageNo' : '4', 
+    'numOfRows' : '1000', 
     'dataType' : 'JSON', 
-    'base_date' : '20230316', 
+    'base_date' : '20230317', 
     'base_time' : '0600',
     'nx' : '98', 
     'ny' : '76'
@@ -24,30 +24,32 @@ total_url = api_url + querySTR
 response = requests.get(total_url)
 ls_dict=json.loads(response.text)
 
-ls_response = ls_dict.get('response')
+print(ls_dict)
 
-ls_body = ls_response.get('body')
+# ls_response = ls_dict.get('response')
 
-ls_items = ls_body.get('items')
-ls_item = ls_items.get('item')
+# ls_body = ls_response.get('body')
 
-
-result = {}
-result_dict = {}
-
-for item in ls_item:
-    result = item
-    print(result)
-    result_dict.setdefault(result.get('category'), result.get('obsrValue'))
+# ls_items = ls_body.get('items')
+# ls_item = ls_items.get('item')
 
 
-print("부산광역시 연제구 동네예보(초단기실황)데이터")
-print("날짜 : "+result.get("baseDate")[:-4]+"년"+result.get("baseDate")[4:-2]+"월"+result.get("baseDate")[6:]+"일"+"시간 : " + result.get("baseTime")[:-2]+"시")
-print("강우형태 : "+result_dict["PTY"])
-print("습도 : "+result_dict["REH"]+" %")
-print("1시간 강수량 : " +result_dict["RN1"]+" mm")
-print("기온 : "+result_dict["T1H"] +" ℃")
-print("동서바람성분 : " +result_dict["UUU"]+" m/s")
-print("남북바람성분 : " + result_dict["VVV"]+" m/s")
-print("풍향 : "+result_dict["VEC"])
-print("풍속 : "+result_dict["WSD"])
+# result = {}
+# result_dict = {}
+
+# for item in ls_item:
+#     result = item
+#     print(result)
+#     result_dict.setdefault(result.get('category'), result.get('obsrValue'))
+
+
+# print("부산광역시 연제구 동네예보(초단기실황)데이터")
+# print("날짜 : "+result.get("baseDate")[:-4]+"년"+result.get("baseDate")[4:-2]+"월"+result.get("baseDate")[6:]+"일"+"시간 : " + result.get("baseTime")[:-2]+"시")
+# print("강우형태 : "+result_dict["PTY"])
+# print("습도 : "+result_dict["REH"]+" %")
+# print("1시간 강수량 : " +result_dict["RN1"]+" mm")
+# print("기온 : "+result_dict["T1H"] +" ℃")
+# print("동서바람성분 : " +result_dict["UUU"]+" m/s")
+# print("남북바람성분 : " + result_dict["VVV"]+" m/s")
+# print("풍향 : "+result_dict["VEC"])
+# print("풍속 : "+result_dict["WSD"])
